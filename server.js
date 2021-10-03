8f3d93f26e791e4d1eb38ecaebae6738a6877c35
Hitesh Mandav <mandavhitesh@gmail.com>
2021-10-04 00:48:37 +0530

    Added socket io connection , send message and recive message event

--- server.js
+++ server.js
@@ -11,6 +11,19 @@ const socket = require('socket.io')(http, {
     }
 });
 
+app.get('/', (req, res) => {
+    res.send('Hello server is running ')
+});
+
+
+socket.on('connection', (connectedSocket) => {
+    console.log(`connection established : ${connectedSocket.id}`);
+    connectedSocket.on('send-message', message => {
+        console.log(`Message Recived ${message}`);
+        connectedSocket.broadcast.emit('recive-message', message);
+    });
+});
+
 http.listen(3000, () => {
     console.log('code colab server is running');
 });
