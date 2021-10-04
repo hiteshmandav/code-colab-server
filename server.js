76227e29441f585db88702f8faafc418c79f2658
Hitesh Mandav <mandavhitesh@gmail.com>
2021-10-04 23:52:24 +0530

    user list add get and remove, privarte message send and recive

--- server.js
+++ server.js
@@ -11,6 +11,28 @@ const socket = require('socket.io')(http, {
     }
 });
 
+let users = [];
+
+const addUser =  (tag, room, id) => {
+    users.push({
+        tagName : tag,
+        roomName : room,
+        id: id
+    });
+
+    console.log(`user added :: ${tag} :: room :: ${room}`)
+}
+
+const removeUserFromRoom =  (id) => {
+    users = users.filter(user => (user.id != id));
+    console.log(`user removed :: ${id}`)
+}
+
+const getAllUsersFromRoom =  (room) => {
+    let roomUsers = users.filter(user => (user.roomName == room));
+    console.log(`users in Room:: ${room} are :: ${roomUsers}`)
+    return roomUsers;
+}
 
 
 app.get('/', (req, res) => {
@@ -19,9 +41,9 @@ app.get('/', (req, res) => {
 
 
 socket.on('connection', (connectedSocket) => {
-    console.log(`connection established : ${connectedSocket.id}
-                Params recived :: ${connectedSocket.handshake.query.roomName} 
-                :: ${connectedSocket.handshake.query.tagName}`);
+    // console.log(`connection established : ${connectedSocket.id}
+    //             Params recived :: ${connectedSocket.handshake.query.roomName} 
+    //             :: ${connectedSocket.handshake.query.tagName}`);
     let roomName = connectedSocket.handshake.query.roomName;
     let tagName = connectedSocket.handshake.query.tagName;
     connectedSocket.on('send-message', message => {
@@ -31,8 +53,36 @@ socket.on('connection', (connectedSocket) => {
 
     connectedSocket.on('join-room', () => {
         connectedSocket.join(roomName);
+        addUser(tagName, roomName, connectedSocket.id);
         connectedSocket.to(roomName).emit('room-joined', tagName);
+        socket.to(roomName).emit('all-users', getAllUsersFromRoom(roomName));
     });
+
+    connectedSocket.on('disconnect', () => {
+        removeUserFromRoom(connectedSocket.id);
+        socket.to(roomName).emit('user-disconnected', tagName);
+        socket.to(roomName).emit('all-users', getAllUsersFromRoom(roomName));
+    });
+
+    connectedSocket.on('send-private-msg', (privateMessageDetails, callbackFn) => {
+        // removeUserFromRoom(connectedSocket.id);
+        socket.to(privateMessageDetails.id).emit('recive-private-message',(
+                                                {
+                                                    'message' : privateMessageDetails.message,
+                                                    'tag': tagName,
+                                                    'senderId' : connectedSocket.id
+                                                }));
+        let user = users.filter(user => user.id == privateMessageDetails.id)[0].tagName;
+        callbackFn(`Private Message to ${user} was sent SuccessFully `);
+    });
+
+
+
+
+    // connectedSocket.on('stream', image => {
+    //     connectedSocket.to(roomName).emit('stream-send', image);
+    // });
+
 });
 
 http.listen(3000, () => {
